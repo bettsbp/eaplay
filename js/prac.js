25093ccd95865a87de6b287426a6ae7952631e39
Brandon Betts and David Ngo <david@>
2018-06-06 11:22:01 -0700

    add useless practice file for personal references

---
+++ js/prac.js
@@ -0,0 +1,34 @@
+function bettsFunc(goTo, incBy) {
+
+  var numbers = [];
+
+  if (numbers < 0) {
+    return;
+    alert("Enter a real fucking number idiot.");
+  }
+
+  for (var index = 0; index <= goTo; index += incBy) {
+    numbers.push(index);
+  }
+
+  return numbers;
+
+}
+
+bettsFunc(30, 5);
+bettsFunc(50, 7);
+
+"iouuaiouf9ue9e939393u39uoaiufdjklf;elkjaosjflkjeiejkfjdls;aaoiue9f9f999f9fueilajkd fjgoat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat gota goat goat fjgoat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat goat"
+
+function goat(a,b) {
+  var a = parseInt($("input#nleh").val());
+  var b = parseInt($("input#kahf").val());
+
+  if (a + b >= 0) {
+    return true;
+  } else {
+    return false; ``
+  }
+}
+
+goat(01232,2);
